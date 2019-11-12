8fd75e1f0451b096818b181148dc35e966085daa
ThinkJarek <jdomanski79@gmail.com>
2019-11-12 21:38:04 +0100

    New class

--- src/main/java/pl/jdomanski/battle_ships/player/ComputerPlayer.java
+++ src/main/java/pl/jdomanski/battle_ships/player/ComputerPlayer.java
@@ -1,19 +1,27 @@
 package pl.jdomanski.battle_ships.player;
 
 import pl.jdomanski.battle_ships.ShipsBoard;
+import pl.jdomanski.battle_ships.Vector;
 
 public class ComputerPlayer extends Player {
 
+	// == fields ==
+	private Vector lastShotVector;
+	private boolean lastShotHit;
+	
 	// == constructor ==
 	public ComputerPlayer(String name, ShipsBoard ownBoard, ShipsBoard enemyBoard) {
 		super(name, ownBoard, enemyBoard);
 		// TODO Auto-generated constructor stub
+		lastShotVector = null;
+		lastShotHit = false;
 	}
 	
 	// == public methods ==
 	@Override
 	public void shoot() {
-		
+		Vector randomVector = getEnemyBoard().getRandomValidVector();
+		getEnemyBoard().shootAt(randomVector);
 	}
 	
 }
